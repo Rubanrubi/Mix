0fab44eef5a7cf33ae3e18ba42bab8091d05a620
Rubanrubi <ruban@sparkouttech.com>
2021-11-12 23:23:07 +0530

    mix demo with all features

---
+++ resources/views/scss.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Scss</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,49 @@
+const mix = require('laravel-mix');
+
+require('laravel-mix-purgecss');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+
+    // Converts Sass to plain css
+    .sass('resources/scss/app.scss', 'public/css')
+
+    // Remove unused css Autodetect
+    .purgeCss({
+        enabled: true,
+    })
+
+    // Copy Image from resources to public while compiling
+    .copy('resources/assets/img/','public/assets/img/')
+
+    // Merge Multiple plain css to single css file in minified manner
+    .styles([
+        'resources/assets/css/app.css',
+        'resources/assets/css/core.css'
+    ], 'public/assets/css/all.css')
+
+    // Merge Multiple plain js to single js file in minified manner
+    .scripts([
+        'resources/assets/js/app.js',
+        'resources/assets/js/core.js'], 'public/assets/js/all.js')
+        .sourceMaps()
+
+    // Code changes will automatically reflect in browser loads when laravel mix buid successfully
+    .browserSync('http://localhost:8000/')
+
+    // In production added version for security
+    if (mix.inProduction()) {
+        mix.version();
+    }
+
+
